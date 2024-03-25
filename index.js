89cee9c6814c3ded49ce3181b33428199e0619fe
Aditya Negandhi <adityanegandhi@Adityas-MacBook-Air.local>
2024-03-25 18:25:23 -0400

    Recording user cam feed and sending it to node server through socket.io in binary stream format

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const SocketIO = require("socket.io");
 
 const port = 3000;
 const app = express();
 const server = http.createServer(app);
+const io = SocketIO(server);
 
 app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("Socket connection: ", socket.id);
+  socket.on("binarystream", (stream) => {
+    console.log("Binary stream: ", stream);
+  });
+});
 
 server.listen(port, () => {
   `Server listening on: ${port}`;
--- public/script.js
+++ public/script.js
@@ -1,9 +1,29 @@
 const userVideo = document.getElementById("user-video");
+const startBtn = document.getElementById("start-btn");
+
+const state = { media: null };
+const socket = io();
 
 window.addEventListener("load", async (e) => {
   const media = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  state.media = media;
   userVideo.srcObject = media;
+});
+
+startBtn.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 2500000,
+    frameRate: 25,
+  });
+
+  mediaRecorder.ondataavailable = (e) => {
+    console.log("Binary stream available: " + e.data);
+    socket.emit("binarystream", e.data);
+  };
+
+  mediaRecorder.start(25);
 });
